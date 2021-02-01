66be2c6ed8964f787eca524475d2ef46ffb05940
2or3 <t.nagashima.cog@gmail.com>
2021-02-02 00:33:14 +0900

    add coin model

--- app/coin/models.py
+++ app/coin/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Coin(models.Model):
+    user_id = models.IntegerField()
+    amount = models.IntegerField()
+
+class CoinTransactions(models.Model):
+    coin_id = models.ForeignKey(Coin, on_delete=models.CASCADE)
+    user_id = models.IntegerField()
+    charge = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
