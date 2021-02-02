e3c8044169f0c7f90d366e2b30dcd81a5ca0d313
2or3 <t.nagashima.cog@gmail.com>
2021-02-02 19:24:11 +0900

    fix coin test to use fixture

--- app/coin/models.py
+++ app/coin/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class CoinManager(models.Manager):
+    def calculate(self, user_id, value):
+        return 10
+
+    def get_amount(self, user_id):
+        result = super().get_queryset().filter(user_id=user_id)
+        return result[0].amount
+
 class Coin(models.Model):
+    objects = CoinManager()
+
     user_id = models.IntegerField()
     amount = models.PositiveIntegerField()
 
@@ -11,4 +21,3 @@ class Transactions(models.Model):
     charge = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
--- app/coin/tests.py
+++ app/coin/tests.py
@@ -4,21 +4,38 @@ from .models import Coin
 
 # Create your tests here.
 class CoinModelTests(TestCase):
-    def test_calculate_coin_with_positive_amount(self):
+    fixtures = ['fixture1']
+
+    def setUp(self):
+        self.user_id = "1234567890"
+        return
+
+    def test_calculate_with_positive_amount(self):
         """
-        calculate_coin() with positive amount returns True if succeeded coin calculation.
+        calculate() with positive amount returns coin amount if succeeded coin calculation.
+        """
+        before_amount = Coin.objects.get_amount(self.user_id)
+        add_coin_value = 100
+        after_amount = Coin.objects.calculate(self.user_id, add_coin_value)
+        self.assertEqual(after_amount, before_amount + add_coin_value)
+
+    def test_calculate_with_minus_amount(self):
+        """
+        calculate() with minus amount returns coin amount if succeeded coin calculation.
+        """
+        before_amount = Coin.objects.get_amount(self.user_id)
+        add_coin_value = -100
+        after_amount = Coin.objects.calculate(self.user_id, add_coin_value)
+        self.assertEqual(after_amount, before_amount + add_coin_value)
+
+    def test_calculate_with_exceeded_minus_amount(self):
+        """
+        calculate() with exceeded minus amount returns False if negative value exceeded coin amount.
         """
         return
 
-    def test_calculate_coin_with_minus_amount(self):
+    def test_get_amount_with_user_id(self):
         """
-        calculate_coin() with minus amount returns True if succeeded coin calculation.
-        """
-        return
-
-    def test_get_coin_with_user_id(self):
-        """
-        get_coin() for specific user returns Valid amount if succeeded coin calculation.
+        get_amount() for specific user returns valid amount if succeeded coin amount get.
         """
         return
-
