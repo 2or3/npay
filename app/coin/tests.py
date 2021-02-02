6f8f5e87991840c7feffbe4c3e79ec36a3ce1530
2or3 <t.nagashima.cog@gmail.com>
2021-02-02 16:31:39 +0900

    add test cases for coin & payment

--- app/coin/tests.py
+++ app/coin/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
+from .models import Coin
+
 # Create your tests here.
+class CoinModelTests(TestCase):
+    def test_calculate_coin_with_positive_amount(self):
+        """
+        calculate_coin() with positive amount returns True if succeeded coin calculation.
+        """
+        return
+
+    def test_calculate_coin_with_minus_amount(self):
+        """
+        calculate_coin() with minus amount returns True if succeeded coin calculation.
+        """
+        return
+
+    def test_get_coin_with_user_id(self):
+        """
+        get_coin() for specific user returns Valid amount if succeeded coin calculation.
+        """
+        return
+
--- app/payment/tests.py
+++ app/payment/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+from .models import Payment
+
 # Create your tests here.
+class PaymentModelTests(TestCase):
+    def test_charge_with_coin(self):
+        """
+        charge('coin') would returns coin balance if succeeded charge.
+        """
+        return
+
+    def test_consume_with_coin(self):
+        """
+        consume('coin') would returns coin balance if succeeded consume.
+        """
+        return
