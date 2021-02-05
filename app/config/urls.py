753ed3fbd204bd85b5d8c4f2df716ef4022ac155
2or3 <t.nagashima.cog@gmail.com>
2021-02-06 00:43:59 +0900

    add redirection to api

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from . import views
 
 import os
 
 app_list = os.getenv('APP_LIST', "").split(",")
 
 urlpatterns = [
+    path('', views.api_redirect),
     path('admin/', admin.site.urls),
 ]
 
---
+++ app/config/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render, redirect
+
+def api_redirect(request):
+    return redirect('/api')
