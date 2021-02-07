b50fc81569cd1e6569046e2cd22b75f58534e6e9
2or3 <t.nagashima.cog@gmail.com>
2021-02-08 00:26:11 +0900

    use fixture for payment test

--- app/payment/tests.py
+++ app/payment/tests.py
@@ -4,6 +4,12 @@ from .models import Transactions
 
 # Create your tests here.
 class PaymentModelTests(TestCase):
+    fixtures = ['fixture1']
+
+    def setUp(self):
+        self.user_id = "1234567890"
+        return
+
     def test_charge_with_coin(self):
         """
         charge('coin') would returns coin balance if succeeded charge.
