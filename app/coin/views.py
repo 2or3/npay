f35421e5dfe80fa9156ea985c62a369d994f8fc1
2or3 <t.nagashima.cog@gmail.com>
2021-02-16 00:09:48 +0900

    fix put method for coin api

--- app/coin/views.py
+++ app/coin/views.py
@@ -1,14 +1,16 @@
 # coding: utf-8
 
 from rest_framework import status, viewsets
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 
 from coin.models import Coin
 from coin.serializers import CoinSerializer
 
 
 @api_view(['GET', 'PUT'])
+@parser_classes([JSONParser])
 def coin_me(request, user_id):
     """
     Get coin balance and modify coin.
@@ -18,9 +20,8 @@ def coin_me(request, user_id):
         return Response(coin_amount)
 
     elif request.method == "PUT":
-        coin = Coin.objects.get_amount(user_id=user_id)
-        serializer = CoinSerializer(coin)
-        return Response(serializer.data)
+        coin = Coin.objects.calculate(user_id=user_id, value=request.data["value"])
+        return Response(coin)
 
 class CoinViewSet(viewsets.ModelViewSet):
     queryset = Coin.objects.all()
