d3beb10c38aa130394a459b771e0d0fef9a948eb
2or3 <t.nagashima.cog@gmail.com>
2021-02-21 00:18:08 +0900

    Add payment api

--- app/payment/urls.py
+++ app/payment/urls.py
@@ -1,8 +1,17 @@
 # coding: utf-8
 
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import routers
-from .views import PaymentViewSet
+from payment import views
+from payment.views import PaymentViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r"payments", PaymentViewSet)
+
+urlpatterns = [
+    path("<int:user_id>/payments", views.users_payments),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- app/payment/views.py
+++ app/payment/views.py
@@ -1,8 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 
-from .models import Transactions
-from .serializers import PaymentSerializer
+from payment.models import Transactions
+from payment.serializers import PaymentSerializer
 
+
+@api_view(["GET", "PUT"])
+@parser_classes([JSONParser])
+def users_payments(request, user_id):
+    """
+    Get payments and charge.
+    """
+    return Response(Transactions.objects.all())
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Transactions.objects.all()
