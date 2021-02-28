91b2983b90293cf932454406f28237d3b8cded08
2or3 <t.nagashima.cog@gmail.com>
2021-02-28 22:37:00 +0900

    Fix payment api test

--- app/payment/tests.py
+++ app/payment/tests.py
@@ -10,14 +10,36 @@ class PaymentModelTests(TestCase):
         self.user_id = "1234567890"
         return
 
-    def test_charge_with_coin(self):
+    def test_create_payment(self):
         """
-        charge('coin') would returns coin balance if succeeded charge.
+        post payment('user', 'amount') would returns true if succeeded charge.
+        """
+        add_coin_value = 100
+        result = Transactions.objects.charge(self.user_id, add_coin_value)
+        self.assertTrue(result)
+
+    def test_create_payment_with_over_amount(self):
+        """
+        post payment('user', 'amount') would returns false with over charge.
+        """
+        add_coin_value = 10000
+        result = Transactions.objects.charge(self.user_id, add_coin_value)
+        self.assertFalse(result)
+
+    def test_update_payment(self):
+        """
+        put payment('transaction', 'amount') would returns payment by transaction.
         """
         return
 
-    def test_consume_with_coin(self):
+    def test_show_payment(self):
         """
-        consume('coin') would returns coin balance if succeeded consume.
+        get payment('transaction') would returns payment by transaction.
+        """
+        return
+
+    def test_list_payment(self):
+        """
+        list_payment('user') would returns all payment list for user.
         """
         return
